335873dc6c3c3f66d34731d05e85057560ee854f
Kamarudheen <kamaru.office@gmail.com>
2025-01-08 20:39:53 +0530

    dashboard statics added

--- Dashboard/views.py
+++ Dashboard/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from Customers.models import Customer, Lead
+from Works.models import Work
 
 # Create your views here.
 
 @login_required
 def dashboard(request):
+    works = Work.active_objects.all()
+    customers = Customer.active_objects.all()
+
     context = {
-        'main' : 'dashboard'
+        'main' : 'dashboard',
+        'customers' : customers,
+        'pending_works' : works.filter(status='PENDING'),
+        'ongoing_works' : works.filter(status='ONGOING'),
+        'completed_works' : works.filter(status='COMPLETED')
     }
     return render(request,'dashboard/index.html',context)
